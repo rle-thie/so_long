2c201a20f19209e6b34425827c78761513a1d833
rle-thie <robinlethiec1@gmail.com>
2022-01-30 18:12:11 +0100

    img

--- srcs/main.c
+++ srcs/main.c
@@ -6,28 +6,28 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:19:24 by rle-thie          #+#    #+#             */
-/*   Updated: 2022/01/30 17:47:02 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/01/30 18:04:40 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-// int main(void)
-// {
-//     void    *mlx_ptr;
-//     void    *win_ptr;
-
-//     mlx_ptr = mlx_init();
-//     win_ptr = mlx_new_window(mlx_ptr, 500, 500, "oui");
-//     mlx_loop(mlx_ptr);
-//     return (0);
-// }
-
 int main(void)
 {
-	t_data	data;
-	
-	init_data(&data);
-	printf("%d", data.length);
-	return(0);
+    void    *mlx_ptr;
+    void    *win_ptr;
+
+    mlx_ptr = mlx_init();
+    win_ptr = mlx_new_window(mlx_ptr, 64, 64, "oui");
+    mlx_loop(mlx_ptr);
+    return (0);
 }
+
+// int main(void)
+// {
+// 	t_data	data;
+	
+// 	init_data(&data);
+// 	printf("%d", data.length);
+// 	return(0);
+// }
